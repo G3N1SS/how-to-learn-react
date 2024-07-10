41c46a07fdcfd9ba49db93d87ccd691575dd2998
unknown <areg.sarkisyan.99@mail.ru>
2024-07-10 22:28:06 +0300

    gh-pages install

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/how-to-learn-react-rework',
   plugins: [react()],
+  base: '/how-to-learn-react-rework'
 })
